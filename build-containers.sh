56efe9662f91c877d2296255586ce07d3b757379
reggiemcdonald <regmcdonald@google.com>
2020-06-24 16:22:11 +0000

    updated to use docker compose build

--- build-containers.sh
+++ build-containers.sh
@@ -6,19 +6,18 @@ if [ -z "$PROJECT_ID" ]; then
 fi
 
 REPO=$1
-VERSION=$2
+TAG=$2
 
 if [ -z "$REPO" ]; then
     echo "missing or invalid param repo entered"
     exit 1
 fi
 
-if [ -z "$VERSION" ]; then
+if [ -z "$TAG" ]; then
     echo "missing or invalid param version"
     exit 1
 fi
 
-docker build -t "${REPO}/${PROJECT_ID}/food-vendor:${VERSION}" -f food-vendor/Dockerfile .
-docker build -t "${REPO}/${PROJECT_ID}/food-supplier:${VERSION}" -f food-supplier/Dockerfile .
-docker build -t "${REPO}/${PROJECT_ID}/food-item-lookup:${VERSION}" -f food-item-lookup/Dockerfile .
-docker build -t "${REPO}/${PROJECT_ID}/food-finder:${VERSION}" -f food-finder/Dockerfile .
+export REPO=$REPO
+export TAG=$TAG
+docker-compose build --parallel
